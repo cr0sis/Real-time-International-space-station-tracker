b2d74dfdad770394cf89f3f6364cb2575ad72d8f
cr0sis <64103294+cr0sis@users.noreply.github.com>
2020-11-03 09:53:33 +0000

    Update ISS.py
    add line drawing history - broken when wrapping through the world on the right side of the screen for now

--- ISS.py
+++ ISS.py
@@ -7,15 +7,18 @@ screen.setup(720,360)
 screen.setworldcoordinates(-180,-90,180,90)
 screen.bgpic("world.png")
 screen.register_shape("iss.gif")
+screen.title("Real time ISS tracker")
 
 iss = turtle.Turtle()
 iss.shape("iss.gif")
 iss.penup()
+iss.pen(pencolor="red", outline=2)
 
-while True:
+while True:    
     location = ISS_Info.iss_current_loc()
     lat = location['iss_position']['latitude']
     lon = location['iss_position']['longitude']
     print("Position: \n latitude: {}, longitude: {}".format(lat,lon))
     iss.goto(float(lon),float(lat))
+    iss.pendown()
     time.sleep(5)

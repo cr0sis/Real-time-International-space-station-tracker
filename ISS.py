b44d1c97da931451bca76976e5a4a81ead5a9ee6
cr0sis <64103294+cr0sis@users.noreply.github.com>
2020-11-03 16:13:37 +0000

    Update ISS.py
    Added function to clear line drawn with spacebar if screen gets too busy over time

--- ISS.py
+++ ISS.py
@@ -12,21 +12,25 @@ screen.title("Real time ISS tracker")
 iss = turtle.Turtle()
 iss.shape("iss.gif")
 
-iss.penup()
-iss.pen(pencolor="red", pensize=1)
+iss.penup()   ### Avoid a line being drawn from initiliation to first coord
+iss.pen(pencolor="red", pensize=1) 
 
+def wipe():
+    iss.clear()
 
-while True:           
+screen.onkey(wipe, "space")### Allow us to clear history with spacebar 
+screen.listen()            ### if screen gets too busy over time
+
+while True:       
     location = ISS_Info.iss_current_loc()
     lat = location['iss_position']['latitude']
     lon = location['iss_position']['longitude']
     print("Position: \n latitude: {}, longitude: {}".format(lat,lon))
     pos = iss.pos() 
     posx = iss.xcor()
-    print(posx)
-    if iss.xcor() >= (179.1):                 ### Lift up at the right edge of  
-        iss.penup()                         ### the screen to avoid drawing a 
-        iss.goto(float(lon),float(lat))     ### horizontal wrap round line
+    if iss.xcor() >= (179.1):           ### Stop drawing at the right edge of  
+        iss.penup()                     ### the screen to avoid a 
+        iss.goto(float(lon),float(lat)) ### horizontal wrap round line
         time.sleep(5)
     else:
       iss.goto(float(lon),float(lat))

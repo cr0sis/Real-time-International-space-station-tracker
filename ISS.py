48f6bca4964721cd10624ac78e5d2101220ac00b
cr0sis <gazman86@gmail.com>
2025-07-09 17:24:51 +0100

    error protection

--- ISS.py
+++ ISS.py
@@ -2,137 +2,85 @@ import ISS_Info
 import turtle
 import time
 import json
-import urllib.request\
+import urllib.request
+import urllib.error  # <-- for catching URL-related errors
 
+# Set up the screen
 screen = turtle.Screen()
-screen.setup(720,360)
-screen.setworldcoordinates(-180,-90,180,90)
+screen.setup(720, 360)
+screen.setworldcoordinates(-180, -90, 180, 90)
 screen.bgpic("map.png")
 screen.bgcolor("black")
-screen.register_shape("isss.gif")
 screen.title("Real time ISS tracker")
 
+# Try registering the shape
+try:
+    screen.register_shape("isss.gif")
+except turtle.TurtleGraphicsError as e:
+    print("Error registering shape: ", e)
+
+# Set up ISS turtle
 iss = turtle.Turtle()
-iss.shape("isss.gif")
-iss.penup()   ### Avoid a line being drawn from initilisation to first coord
+try:
+    iss.shape("isss.gif")
+except turtle.TurtleGraphicsError:
+    iss.shape("circle")  # Fallback shape
+    print("Using default shape instead of 'isss.gif'")
+iss.penup()
 iss.pen(pencolor="red", pensize=1)
 style = ('Arial', 8, 'bold')
 
-'''# Home
-lat = 54.06459 
-lon = -2.9062
-
-prediction = turtle.Turtle()
-prediction.penup()
-prediction.color('yellow')
-prediction.goto(lon,lat)
-prediction.dot(5)
-prediction.hideturtle()
-
-url = 'http://api.open-notify.org/iss-now.json?lat=' +str(lat) + '&lon=' + str(lon)
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
-
-#over = result ['response'][1]['risetime']
-
-#prediction.write(time.ctime(over), font=style)
-
-
- Houston
-lat = 29.640427
-lon = -95.279804
-
-prediction.penup()
-prediction.color('orange')
-prediction.goto(lon,lat)
-prediction.dot(5)
-prediction.hideturtle()
-
-url = 'http://api.open-notify.org/iss-now.json?lat=' +str(lat) + '&lon=' + str(lon)
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
-
-#over = result ['response'][1]['risetime']
-
-#prediction.write(time.ctime(over), font=style)
-
-# Monqui
-lat = 45.08003
-lon = -93.2967
-
-prediction.penup()
-prediction.color('pink')
-prediction.goto(lon,lat)
-prediction.dot(5)
-prediction.hideturtle()
-
-url = 'http://api.open-notify.org/iss-now.json?lat=' +str(lat) + '&lon=' + str(lon)
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
-
-#over = result ['response'][1]['risetime']
-
-#prediction.write(time.ctime(over), font=style)
-
-# Cunin
-lat = 45.9144
-lon = 13.2752
-
-prediction.penup()
-prediction.color('light blue')
-prediction.goto(lon,lat)
-prediction.dot(5)
-prediction.hideturtle()
-
-url = 'http://api.open-notify.org/iss-now.json?lat=' +str(lat) + '&lon=' + str(lon)
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
-
-#over = result ['response'][1]['risetime']
-
-#prediction.write(time.ctime(over), font=style) '''
-
+# Set up astronauts info turtle
 astronauts = turtle.Turtle()
 astronauts.penup()
-astronauts.color('black')
-astronauts.goto(-178,86)
+astronauts.color('orange')
+astronauts.goto(-178, 0)
 astronauts.hideturtle()
+
+# Fetch astronaut data
 url = "http://api.open-notify.org/astros.json"
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
-print("There are currently " + str(result["number"]) + " astronauts in space:")
-print("")
-astronauts.write("People in space: " + str(result["number"]), font=style)
-astronauts.sety(astronauts.ycor() - 5)
-
-people = result["people"]
-
-for p in people:
-    print(p["name"] + " on: " + p["craft"])
-    astronauts.write(p["name"] + " on: " + p["craft"], font=style)
+try:
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+    print("There are currently " + str(result["number"]) + " astronauts in space:\n")
+    astronauts.write("People in space: " + str(result["number"]), font=style)
     astronauts.sety(astronauts.ycor() - 5)
 
+    for p in result.get("people", []):
+        print(p["name"] + " on: " + p["craft"])
+        astronauts.write(p["name"] + " on: " + p["craft"], font=style)
+        astronauts.sety(astronauts.ycor() - 5)
+except urllib.error.URLError as e:
+    print("Failed to fetch astronaut data:", e)
+    astronauts.write("Error fetching astronaut data", font=style)
+except json.JSONDecodeError as e:
+    print("Failed to decode astronaut JSON:", e)
+
+# Clear ISS path with spacebar
 def wipe():
     iss.clear()
 
-screen.onkey(wipe, "space")### Allow us to clear history with spacebar 
-screen.listen()            ### if screen gets too busy over time
+screen.onkey(wipe, "space")
+screen.listen()
 
-while True:  
-     
-    location = ISS_Info.iss_current_loc()
-    lat = location['iss_position']['latitude']
-    lon = location['iss_position']['longitude']
-    print("Position: \n latitude: {}, longitude: {}".format(lat,lon))
-    pos = iss.pos() 
-    posx = iss.xcor()
-    if iss.xcor() >= (179.1):           ### Stop drawing at the right edge of  
-        iss.penup()                     ### the screen to avoid a 
-        iss.goto(float(lon),float(lat)) ### horizontal wrap round line
-        time.sleep(5)
-    else:
-      iss.goto(float(lon),float(lat))
-      iss.pendown()
-      time.sleep(5)
+# Main loop
+while True:
+    try:
+        location = ISS_Info.iss_current_loc()
+        lat = float(location['iss_position']['latitude'])
+        lon = float(location['iss_position']['longitude'])
 
- 
+        print("Position: \n latitude: {}, longitude: {}".format(lat, lon))
+
+        if iss.xcor() >= 178:
+            iss.penup()
+            iss.goto(lon, lat)
+        else:
+            iss.goto(lon, lat)
+            iss.pendown()
+    except (KeyError, TypeError, ValueError) as e:
+        print("Error in ISS location data:", e)
+    except Exception as e:
+        print("Unexpected error:", e)
+
+    time.sleep(30)
